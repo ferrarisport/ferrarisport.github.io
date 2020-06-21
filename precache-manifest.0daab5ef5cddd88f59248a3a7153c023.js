2a81d09349290841e46971e36587545a7960c35d
ferrarisport <52331550+ferrarisport@users.noreply.github.com>
2020-06-21 16:22:06 +0300

    TOS

---
+++ precache-manifest.0daab5ef5cddd88f59248a3a7153c023.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca238cf0a20a2a6de676a2a14b9cb1cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "674256139b6fb5e9927d",
+    "url": "/static/css/main.68627df0.chunk.css"
+  },
+  {
+    "revision": "838df69b07978989f7b1",
+    "url": "/static/js/2.cc878a10.chunk.js"
+  },
+  {
+    "revision": "c21a78784305bd0b125a86fae7d9b7e4",
+    "url": "/static/js/2.cc878a10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "674256139b6fb5e9927d",
+    "url": "/static/js/main.e18da7b2.chunk.js"
+  },
+  {
+    "revision": "a7219b4b5712152cf6a1",
+    "url": "/static/js/runtime-main.862e3eb3.js"
+  }
+]);
